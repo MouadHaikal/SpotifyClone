dbd1c91e919d3d6bf84a37e55fb1aa86229106d4
Mouad Haikal <Mouad.Haikal@um6p.ma>
2025-05-11 18:27:56 +0100

    Project setup

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <Header />
+
+    <div>
+        <slot />
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image']
-})
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', 'shadcn-nuxt'],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    },
+    css: ['~/assets/css/main.css'],
+    vite: {
+        plugins: [ tailwindcss(), ],
+    },
+});
